295629f012436f2a528a7b160b34aee3a9da2d58
Rassul-creator-commits <rassulk@nycstudents.net>
2026-03-17 12:32:11 -0400

    asdf

--- vue-open-data-project/src/App.vue
+++ vue-open-data-project/src/App.vue
@@ -1,20 +1,37 @@
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <DeathCard v-for="(mon, index) in death" :key="mon.name" :Death="mon" :id="index + 1" />
+  </div>
 </template>
-<script setup>
-import { createRouter } from 'vue'
 
-const router = createRouter({
-  routes: [
-    { path: '/', component: HomeView },
-    { path: '/PokemonData/:id', component: PokemonData },
-  ],
+<script setup>
+import { ref, onMounted } from 'vue'
+
+import DeathCard from '../components/DeathCard.vue'
+const death = ref([])
+async function getDeathRate() {
+  try {
+    const response = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
+
+    const data = await response.json()
+    death.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
+onMounted(() => {
+  getDeathRate()
 })
 </script>
 
-// fetch (https://data.cityofnewyork.us/api/v3/views/jb7j-dtam/query.json)
-<style scoped></style>
+<style scoped>
+.container {
+  width: 80vw;
+  margin: 30px auto;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+</style>
