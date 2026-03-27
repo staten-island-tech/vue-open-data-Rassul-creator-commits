9eb8ebb491cd3818b39e5a5b06637a43a26506c1
Rassul-creator-commits <rassulk@nycstudents.net>
2026-03-27 12:31:49 -0400

    asdf

--- vue-open-data-project/src/router/index.js
+++ vue-open-data-project/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from './views/HomeView.vue'
+import DeathVue from './views/DeathVue.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
-    { path: '/', name: home, component: HomeView },
-    { path: '/death', component: DeathView.component },
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/death', component: DeathVue },
   ],
 })
-
 export default router
